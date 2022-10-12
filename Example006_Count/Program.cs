fb8f3afff79e3671e4a1f02b4d7e73d9592d61aa
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-12 20:28:07 +0300

    Добавили изменения в пример 6 COUNT

---
+++ Example006_Count/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 2;
+int c = 12;
+int d = 8;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
