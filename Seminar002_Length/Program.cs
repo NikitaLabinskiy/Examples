c2007cd5d51c34e63de4427442ca905257c4f4de
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-15 21:39:14 +0300

    Добавили задачу с размером МАССИВА

---
+++ Seminar002_Length/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+//              0   1   2    3   4   5   6   7   8
+int[] array = { 1, 23, 34, 53, 255, 63, 53, 95, 34, 25 };
+
+int n = array.Length;
+
+int find = 34;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.Write(index);
+        break;
+    }
+    index++;
+} 
