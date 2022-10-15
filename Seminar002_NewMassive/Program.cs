d42a1c2dbf65d1258ed211d1f8f597aa8a708ba8
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-15 23:08:16 +0300

    Добавили задачу с Функциями МАССИВА

---
+++ Seminar002_NewMassive/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Clear();
+
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        Console.Write(", ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[20];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 8);
+Console.WriteLine(pos);
