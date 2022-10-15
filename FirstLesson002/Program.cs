64884d455d2d9410c74fb86a5a97837bac80e066
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-15 19:20:11 +0300

    задача 4 к первому семинару(Дополнение)

--- FirstLesson002/Program.cs
+++ FirstLesson002/Program.cs
@@ -1,6 +1,22 @@
 ﻿Console.Clear();
-Console.Write("Введите число ");
+//Console.Write("Введите число ");
 
-int N = Convert.ToInt32(Console.ReadLine());
+//int N = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(N % 10);
+//Console.WriteLine(N % 10);
+
+Console.Write("Введите два числа: ");
+
+int NumberA = Convert.ToInt32(Console.ReadLine());
+int NumberB = Convert.ToInt32(Console.ReadLine());
+
+int square = NumberA * NumberA;
+
+if (square == NumberB)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
