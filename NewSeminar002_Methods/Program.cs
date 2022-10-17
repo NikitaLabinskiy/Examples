9b8c2f3d45b7437af39aae5ac44d4c46c1668ec6
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-17 21:51:46 +0300

    Разбор семинара 2, первая задача

---
+++ NewSeminar002_Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+
+int GetRandomNumber(int minValue, int maxValue)
+{
+    int randomNumber = new Random().Next(minValue, maxValue);
+    return randomNumber;
+}
+
+
+int FindMaxWalue(int number)
+{
+    int max = number % 10;
+    number = number / 10;
+
+   while (number > 0)
+   {
+      int lastNumber = number % 10;
+      if(lastNumber > max)
+      {
+         max = lastNumber;
+      }
+      number /= 10;
+   }
+   return max;
+}
+
+int searchNumber = GetRandomNumber(10, 1000);
+
+Console.WriteLine(searchNumber);
+
+int maxValue = FindMaxWalue(searchNumber);
+
+Console.WriteLine(maxValue);
