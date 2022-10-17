000433610169a7fc725a8f9975ed0226f46888bf
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-17 23:04:45 +0300

    Разбор семинара 2, вторая задача

---
+++ NewSeminar002_Methods_Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int GetRandomNumber(int minValue, int maxValue)
+{
+    int randomNumber = new Random().Next(minValue, maxValue);
+    return randomNumber;
+}
+
+
+int DeletesecondDigit(int number)
+{
+    int lastDigit = number % 10;
+    number = number / 100;
+    int resault = number * 10 + lastDigit;
+    return resault;
+}
+
+int something = GetRandomNumber(100, 1000);
+
+Console.WriteLine(something);
+
+int something2 = DeletesecondDigit(something);
+
+Console.WriteLine(something2);
