6478f4a2fcac11236fef1566b582d16b97043fa6
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-18 16:16:23 +0300

    Разбор семинара 2, двенадцатая задача

---
+++ NewSeminar002_Methods_Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+void Comparison (int firstNumber, int secondNumber)
+{
+    // Console.Write("Введите два числа");
+    // firstNumber = Convert.ToInt32(Console.ReadLine());
+    // secondNumber = Convert.ToInt32(Console.ReadLine());
+   
+
+    if (firstNumber % 2 == 0 & secondNumber % 2 == 0)
+    {
+        Console.WriteLine("Кратное");
+    }
+    else
+    {
+        Console.Write("Некратное, остаток ");
+        Console.Write(firstNumber % secondNumber);
+    }
+}
+Comparison(16, 6);
