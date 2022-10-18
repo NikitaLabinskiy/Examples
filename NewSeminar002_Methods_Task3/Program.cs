3c44a7835ba9a46c4a68f1eaf342a9a37a626749
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-18 16:31:08 +0300

    Разбор семинара 2, четырнадцатая задача

---
+++ NewSeminar002_Methods_Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+void Comparison (int number)
+{  
+    if (number % 7 == 0 & number % 23 == 0)
+    {
+        Console.WriteLine("Кратное");
+    }
+    else
+    {
+        Console.Write("Некратное");
+    }
+}
+Comparison(46);
