b2e1e36fb02bcd43b268577e55939980f29ff25b
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-18 17:08:50 +0300

    Разбор семинара 2, шестнадцатая задача

---
+++ NewSeminar002_Methods_Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+void Comparison (int firstNumber, int secondNumber)
+{
+    int squareFirst = firstNumber * firstNumber;
+    int squareSecond = secondNumber * secondNumber;
+
+    if (squareFirst == secondNumber | squareSecond == firstNumber)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+Comparison(25, 625);
