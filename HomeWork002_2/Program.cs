74b6e63a4d68fd4cc72516901cf642d1eb0d0ded
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-21 18:25:54 +0300

    Домашняя работа к 2 семинару, задача 13 попытка 1

---
+++ HomeWork002_2/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.Clear();
+
+int[] array = new int[5];
+int number = 12345;
+
+if (number > 100 & number < 999)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        int lastValue = number % 10;
+        array[i] = lastValue;
+
+        number /= 10;
+    }
+}
+
+// for( int i = array.Lanth - 1; i >= 0; i--)
+// {
+//     Console.Write(array[i]);
+// }
+
+// void Reversal (int[] newArray)
+// {
+//     for (int i = 0; i = array.Length / 2; i++)
+//     {
+//         int help = 0;
+//         help = array[i];
+//         array[i] = array[array.Length - 1 - i];
+//         array[array.Length - 1 - i] = help;
+//     }
+// }
+
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position]);
+        Console.WriteLine(", ");
+        position++;
+    }
+}
+
+
+PrintArray(array);
+
+
+
+
+
+
+
+
