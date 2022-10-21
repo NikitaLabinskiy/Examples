5f57a440e360ca2254f870a818c9349f8455b5df
Nikita Labinskiy <nlabinskiy@gmail.com>
2022-10-21 18:26:08 +0300

    Домашняя работа к 2 семинару, задача 13 попытка 2

---
+++ HomeWork002_02/Program.cs
@@ -0,0 +1,55 @@
+﻿Console.Clear();
+
+// int[] array = new int[3];
+int number = 867665;
+
+
+while(number > 999 | number < 100)
+{
+    if(number > 999)
+    {
+        number /= 10;
+    }
+    if(number < 100)
+    {
+        Console.Write("Третьей цифры нет!");
+        break;
+    }
+    if (number < 999 & number >= 100)
+    {
+        Console.Write(number % 10);
+    }
+}
+
+// if (number > 999)
+// {
+//     while(number > 999)
+//     {
+//         number /= 10;
+//     }
+// }
+//  else
+// {
+//     if (number >= 100)
+//    {
+//         number = number % 10;
+//         Console.WriteLine(number);
+//    } 
+//    else
+//    {
+//         Console.WriteLine("числа нет");
+//    }
+// }
+
+
+// Console.WriteLine(number);
+
+// for(int i = 0; i < array.Length; i++)
+// {
+//     int lastValue = number % 10;
+//     array[i] = lastValue;
+
+//     number /= 10;
+// }
+
+// Console.WriteLine(array[0]);
